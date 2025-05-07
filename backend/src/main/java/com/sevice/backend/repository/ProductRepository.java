cb0466753e1ea1cc7f5a4b53fe95878812a33cb5
lazy34 <maitysurajit0901@gmail.com>
2025-05-08 00:50:13 +0530

    Modify Product repository

--- backend/src/main/java/com/sevice/backend/repository/ProductRepository.java
+++ backend/src/main/java/com/sevice/backend/repository/ProductRepository.java
@@ -2,11 +2,17 @@ package com.sevice.backend.repository;
 
 import com.sevice.backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
-    List<Product> findByNameContainingIgnoreCaseOrBrandContainingIgnoreCase(String name, String brand);
+
+    @Query("SELECT p FROM Product p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<Product> searchProducts(@Param("query") String query);
 }
