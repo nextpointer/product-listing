ab06c0de89e63b13562c27e3ff4100b42ab04cd8
lazy34 <maitysurajit0901@gmail.com>
2025-05-08 00:54:04 +0530

    Modify Product Not found exception

--- backend/src/main/java/com/sevice/backend/exception/ProductNotFoundException.java
+++ backend/src/main/java/com/sevice/backend/exception/ProductNotFoundException.java
@@ -1,7 +1,9 @@
 package com.sevice.backend.exception;
 
+import java.util.UUID;
+
 public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
+    public ProductNotFoundException(UUID id) {
+        super("Product not found with ID: " + id);
     }
 }
