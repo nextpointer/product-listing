8905d743b528e243fd055f2b5d4f8fd19d0dcf36
lazy34 <maitysurajit0901@gmail.com>
2025-05-07 13:31:08 +0530

    Add cloudinary config for store images

---
+++ backend/src/main/java/com/sevice/backend/config/CloudinaryConfig.java
@@ -0,0 +1,28 @@
+package com.sevice.backend.config;
+
+import com.cloudinary.Cloudinary;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "cloudinary")
+public class CloudinaryConfig {
+
+    private String cloudName;
+    private String apiKey;
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        return new Cloudinary(config);
+    }
+}
